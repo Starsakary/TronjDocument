31e29e943394cbe8232e11a8f838e371d71738a1
Sakary Jia <sakary.jia@tron.network>
2020-12-02 22:43:22 +0800

    no message

--- conf.py
+++ conf.py
@@ -10,16 +10,13 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('.'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
 
-html_theme = 'murray'
-import murray
-html_theme_path = [murray.get_html_theme_path()]
 
 project = 'Tronj'
 copyright = '2020, Sakary'
@@ -53,7 +50,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'jupyter-sphinx-theme'
+html_theme = 'sphinx_materialdesign_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
